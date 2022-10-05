ca2ce878433664a907fa757d52f361d04525003a
Godience <115064790+Godience@users.noreply.github.com>
2022-10-05 14:59:10 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+let go = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 6; index++) {
+        go += 1
+        basic.showNumber(go)
+    }
 })
